afc458c8e0cfa16ee1de21eca9d947230e0c8d4f
= <=>
2021-12-01 10:42:45 +0400

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public delegate int MathHandler(int a, int b); 
+    class Program
+    {
+        public static int Sum(int a, int b)
+        {
+            Console.WriteLine("Sum starts");
+            return a + b;
+        }
+        public static int Difference(int a, int b)
+        {
+            Console.WriteLine("Difference starts");
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+            MathHandler mathHandler = new MathHandler(Sum);
+            mathHandler += Difference;
+            MathHandler mathHandler2 = new MathHandler(Sum);
+            
+            MyMath test = new MyMath();
+            test.MathEvent += mathHandler;
+            test.MathEvent += mathHandler2;
+            test.DoWork(5,2);
+        }
+    }
+    class MyMath
+    {
+        public event MathHandler MathEvent;
+        public void DoWork(int a, int b)
+        {
+            if (MathEvent != null)
+            {
+                foreach (var item in MathEvent.GetInvocationList())
+                {
+                    Console.WriteLine(item.DynamicInvoke(a,b));
+                }
+            }
+        }
+    }
+}
